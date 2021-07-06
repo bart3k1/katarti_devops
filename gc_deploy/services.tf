b6fd4d231b6fccfd8a5d8f3e18a20025ed015505
bart3k1 <bart3k1@gmail.com>
2021-07-06 10:47:27 +0200

    walka z ingresem

--- gc_deploy/services.tf
+++ gc_deploy/services.tf
@@ -1,44 +1,47 @@
-resource "kubernetes_service" "app" {
+resource "kubernetes_service" "falcon-svc" {
   metadata {
-    name = var.app
+    name = "falcon-svc"
     namespace = kubernetes_namespace.bw-namespace.metadata.0.name
     
   }
   spec {
-    selector = {
-      app = kubernetes_deployment.app.metadata.0.labels.app
-    }
+    # selector = {
+    #   app = kubernetes_deployment.app.metadata.0.labels.app
+    # }
     port {
       
       port        = 80
       target_port = 8000
+      protocol = "TCP"
     }
 
-    # type = "NodePort"
-    type = "LoadBalancer"
+    type = "NodePort"
+    # type = "LoadBalancer"
   }
 } 
 
-# resource "kubernetes_ingress" "ingress" {
-#   wait_for_load_balancer = true
-#   metadata {
-#     name = "ingress"
-#     namespace = kubernetes_namespace.bw-namespace.metadata.0.name
-#     annotations = {
-#       "kubernetes.io/ingress.class" = "nginx"
-#     }
-#   }
-#   spec {
-#     rule {
-#       http {
-#         path {
-#           path = "/*"
-#           backend {
-#             service_name = kubernetes_service.app.metadata.0.name
-#             service_port = 8000
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+resource "kubernetes_ingress" "ingress" {
+  wait_for_load_balancer = true
+  metadata {
+    name = "ingress"
+    namespace = kubernetes_namespace.bw-namespace.metadata.0.name
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+      "alb.ingress.kubernetes.io/scheme"      = "internet-facing"
+      "alb.ingress.kubernetes.io/target-type" = "ip"
+    }
+  }
+  spec {
+    rule {
+      http {
+        path {
+          path = "/*"
+          backend {
+            service_name = kubernetes_service.falcon-svc.metadata.0.name
+            service_port = 80
+          }
+        }
+      }
+    }
+  }
+}
